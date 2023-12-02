85171fec6adc91098a7477a857d295614b7465f4
Gabriel Maglia <gab.maglia@gmail.com>
2023-12-02 19:22:12 -0300

    Creados modelos de User, UserCredentials,UserAddress,UserRoles e inicializados en sequelize con sus relaciones

---
+++ Api/src/models/userModels/User.js
@@ -0,0 +1,45 @@
+
+const {DataTypes} = require("sequelize")
+
+module.exports = (sequelize) => {
+    sequelize.define('User', {
+        user_id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+          },
+          name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          surname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          birthdate: {
+            type: DataTypes.DATE,
+            allowNull: true,
+          },
+          dni: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+          },
+          email: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+            isEmail: true,
+          },
+          telephone: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true, 
+          },
+          image: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true, 
+          }
+    }) 
+}
---
+++ Api/src/models/userModels/UserCredentials.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("UserCredentials", {
+    credential_id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
