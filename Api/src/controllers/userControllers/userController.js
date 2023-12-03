c18d634651336b5e41f01ded5c8ae82caa508478
Gabriel Maglia <gab.maglia@gmail.com>
2023-12-03 19:20:52 -0300

    controladores de usuario declarados

--- Api/src/controllers/userControllers/userController.js
+++ Api/src/controllers/userControllers/userController.js
@@ -1,7 +1,22 @@
-const getUserById = async ()=> {
+const getAllUsers = async ()=> {
+    console.log('soyTodos')
+}
+const getUserById = async (id)=> {
     console.log('soyUnUser')
+}
+const postUser = async ()=> {
+    console.log('postUser')
+}
+const editUserById = async (id)=> {
+    console.log('editUserbyId')
+}
+const deleteUserById = async (id)=> {
+    console.log('deleteUserbyId')
 }
 
 module.exports = {
-    getUserById
+    getAllUsers,
+    getUserById,
+    editUserById,
+    deleteUserById
 }
