aaa1f51d046a4e044e7831e100b5e58f41095f4f
MariangelRondan <rondanmariangel@gmail.com>
2023-12-04 13:06:06 -0300

    product handler

--- Api/src/models/productModels/ProductImg.js
+++ Api/src/models/productModels/ProductImg.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("IMG", {
+  sequelize.define("img", {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
