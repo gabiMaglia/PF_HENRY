b7fbb1168ae567da53f69929aeefd9b156bd9e9c
valentin di domenica <valendido69@gmail.com>
2023-12-03 22:39:55 -0300

    add Service model and relations

--- Api/src/db.js
+++ Api/src/db.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const UserModel = require('./models/userModels/User')
-const UserCredentialsModel = require('./models/userModels/UserCredentials')
-const UserRoleModel = require('./models/userModels/UserRole')
-const UserAddressModel = require('./models/userModels/UserAddress')
-const ServiceStatusModel =require('./models/ServiceModels/Service_status')
-
+const UserModel = require("./models/userModels/User");
+const UserCredentialsModel = require("./models/userModels/UserCredentials");
+const UserRoleModel = require("./models/userModels/UserRole");
+const UserAddressModel = require("./models/userModels/UserAddress");
+const ServiceStatusModel = require("./models/ServiceModels/Service_status");
+const ServiceModel = require("./models/ServiceModels/Service");
 const { DB_USER, DB_PASSWORD, DB_HOST, BDD } = process.env;
 
 const sequelize = new Sequelize(
@@ -18,11 +18,12 @@ const sequelize = new Sequelize(
 );
 
 // INICIALISAMOS LOS MODELOS
-UserModel(sequelize)
-UserCredentialsModel(sequelize)
-UserRoleModel(sequelize)
-UserAddressModel(sequelize)
-ServiceStatusModel(sequelize)
+UserModel(sequelize);
+UserCredentialsModel(sequelize);
+UserRoleModel(sequelize);
+UserAddressModel(sequelize);
+ServiceStatusModel(sequelize);
+ServiceModel(sequelize);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
@@ -30,8 +31,9 @@ const {
   User,
   UserRole,
   UserAddress,
-  UserCredentials
-  
+  UserCredentials,
+  Service,
+  Service_status,
 } = sequelize.models;
 
 // RELACIONES
@@ -47,9 +49,30 @@ User.hasOne(UserAddress, {
 UserAddress.belongsTo(User);
 
 User.belongsTo(UserRole);
+
 UserRole.hasMany(User);
-
-
+//service relations
+Service.hasOne(User, {
+  as: "Client",
+  foreignKey: "userId",
+  constraints: false,
+  scope: {
+    role_name: "client",
+  },
+  onDelete: "CASCADE",
+});
+Service.hasOne(Service_status, {
+  onDelete: "CASCADE",
+});
+Service.hasOne(User, {
+  as: "Technician",
+  foreignKey: "technicianId",
+  constraints: false,
+  scope: {
+    role_name: "technician",
+  },
+  onDelete: "CASCADE",
+});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
