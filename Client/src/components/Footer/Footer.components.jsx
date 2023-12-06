28153ec996a415750ea81f163bf9d14b538078fd
Simply92 <pedrouner92@gmail.com>
2023-12-06 10:53:35 -0300

    actualizacion home links

--- Client/src/components/Footer/Footer.components.jsx
+++ Client/src/components/Footer/Footer.components.jsx
@@ -1,29 +1,46 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { Container, Typography, Box } from "@mui/material";
-import { AccessTimeFilled, Room, Email, Phone, Copyright, Inventory, SupportAgent, Help, Language } from '@mui/icons-material';
+import {
+  AccessTimeFilled,
+  Room,
+  Email,
+  Phone,
+  Copyright,
+  Inventory,
+  SupportAgent,
+  Help,
+  Language,
+} from "@mui/icons-material";
 
-import PATHROUTE from "../../helpers/pathRoute"
+import PATHROUTE from "../../helpers/pathRoute";
 
 const FooterComponents = () => {
-  const sectionInformation = [{
-    title: "INFORMACION",
-    items: [
-      { icon: Phone, text: "+54 11 2256-8888" },
-      { icon: Room, text: "Calle Falsa 123 - Buenos Aires" },
-      { icon: Email, text: "contacto@hypermegared.com.ar" },
-      { icon: AccessTimeFilled, text: "Lunes a Viernes: 10 a 14hs y 17 a 20hs | Sábados: 10 a 14hs." },
-    ]
-  }];
+  const sectionInformation = [
+    {
+      title: "INFORMACION",
+      items: [
+        { icon: Phone, text: "+54 11 2256-8888" },
+        { icon: Room, text: "Calle Falsa 123 - Buenos Aires" },
+        { icon: Email, text: "contacto@hypermegared.com.ar" },
+        {
+          icon: AccessTimeFilled,
+          text: "Lunes a Viernes: 10 a 14hs y 17 a 20hs | Sábados: 10 a 14hs.",
+        },
+      ],
+    },
+  ];
 
-  const sectionPages = [{
-    title: "PAGINAS",
-    items: [
-      { icon: Language, text: "INICIO", link: PATHROUTE.HOME },
-      { icon: Inventory, text: "PRODUCTOS", link: PATHROUTE.PRODUCTS },
-      { icon: SupportAgent, text: "SOPORTE", link: PATHROUTE.SUPPORT },
-      { icon: Help, text: "PREGUNTAS FRECUENTES", link: PATHROUTE.QUESTIONS },
-    ]
-  }];
+  const sectionPages = [
+    {
+      title: "PAGINAS",
+      items: [
+        { icon: Language, text: "INICIO", link: PATHROUTE.HOME },
+        { icon: Inventory, text: "PRODUCTOS", link: PATHROUTE.PRODUCTS },
+        { icon: SupportAgent, text: "SOPORTE", link: PATHROUTE.SUPPORT },
+        { icon: Help, text: "PREGUNTAS FRECUENTES", link: PATHROUTE.QUESTIONS },
+      ],
+    },
+  ];
 
   const boxStyle = {
     backgroundColor: "#000",
@@ -41,21 +58,21 @@ const FooterComponents = () => {
   const boxStyleTwo = {
     color: "#fff",
     padding: "30px",
-  }
+  };
 
   const boxStyleThree = {
     color: "#fff",
     display: "flex",
     flexDirection: "column",
     alignItems: "flex-end",
-    padding: "30px"
-  }
+    padding: "30px",
+  };
 
   const boxStyleCopyright = {
     backgroundColor: "#000",
     display: "flex",
     justifyContent: "center",
-    padding: "0 0 30px 0"
+    padding: "0 0 30px 0",
   };
 
   return (
@@ -65,10 +82,19 @@ const FooterComponents = () => {
           {sectionInformation.map((section, index) => (
             <Container key={index}>
               <Box style={boxStyleTwo}>
-                <Typography variant="h5" sx={{ fontWeight: "800", marginBottom: "25px" }}>{section.title}</Typography>
+                <Typography
+                  variant="h5"
+                  sx={{ fontWeight: "800", marginBottom: "25px" }}
+                >
+                  {section.title}
+                </Typography>
                 {section.items.map((item, i) => (
                   <Box key={i} sx={{ display: "flex", marginBottom: "10px" }}>
-                    {item.icon && <item.icon sx={{ color: "#fd611a", marginRight: "10px" }} />}
+                    {item.icon && (
+                      <item.icon
+                        sx={{ color: "#fd611a", marginRight: "10px" }}
+                      />
+                    )}
                     <Typography>{item.text}</Typography>
                   </Box>
                 ))}
@@ -78,11 +104,18 @@ const FooterComponents = () => {
           {sectionPages.map((section, index) => (
             <Container key={index}>
               <Box style={boxStyleThree}>
-                <Typography variant="h5" sx={{ fontWeight: "800", marginBottom: "25px" }}>{section.title}</Typography>
+                <Typography
+                  variant="h5"
+                  sx={{ fontWeight: "800", marginBottom: "25px" }}
+                >
+                  {section.title}
+                </Typography>
                 {section.items.map((item, i) => (
                   <Box key={i} sx={{ display: "flex", marginBottom: "10px" }}>
                     <Link to={item.link} style={{ textDecoration: "none" }}>
-                      <Typography sx={{ marginRight: "10px", color: "#fff" }}>{item.text}</Typography>
+                      <Typography sx={{ marginRight: "10px", color: "#fff" }}>
+                        {item.text}
+                      </Typography>
                     </Link>
                     {item.icon && <item.icon sx={{ color: "#fd611a" }} />}
                   </Box>
@@ -95,7 +128,10 @@ const FooterComponents = () => {
       <Box style={boxStyleCopyright}>
         <Box sx={{ display: "flex" }}>
           <Copyright sx={{ color: "#fd611a", marginRight: "10px" }} />
-          <Typography sx={{ color: "#787373", fontWeight: "500" }}>2023 | Todos los derechos reservados. | Desarrollado por Grupo PF - Henry.</Typography>
+          <Typography sx={{ color: "#787373", fontWeight: "500" }}>
+            2023 | Todos los derechos reservados. | Desarrollado por Grupo 06 PF
+            - Henry.
+          </Typography>
         </Box>
       </Box>
     </>
