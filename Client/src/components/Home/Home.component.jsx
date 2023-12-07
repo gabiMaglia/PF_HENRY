03efb7cb0959649ead1fbbc7b92a1896a4e9dad6
Sebaroca00 <sebaroca00@gmail.com>
2023-12-07 17:14:18 -0300

    estilo homeProductComponent

--- Client/src/components/Home/Home.component.jsx
+++ Client/src/components/Home/Home.component.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import HomeProducts from "../HomeProducts/HomeProducts.component";
 import data from "../../dataBase/bdd.json";
-import { Typography } from "@mui/material";
 
 const HomeComponent = () => {
   const { products } = data;
 
   return (
-    <>
-      <div>
-        <Typography paddingLeft={8} margin={2} fontWeight={"bold"}>
-          ÚLTIMAS NOVEDADES
-        </Typography>
-        <HomeProducts products={products} />
-      </div>
-    </>
+    <div>
+      <HomeProducts products={products} />
+    </div>
   );
 };
 
