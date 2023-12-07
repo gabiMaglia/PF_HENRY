ff9ac4e561ecb9558c575dd241bcef9bbf8a1261
Gabriel Maglia <gab.maglia@gmail.com>
2023-12-07 09:16:33 -0300

    custom hook para trabajar con localStorage agregado

--- Api/src/routes/userRouter/userRouter.js
+++ Api/src/routes/userRouter/userRouter.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const {
   getUsersHandler,
+  getUsersByRoleHandler,
   getUserByIdHandler,
   postUserHandler,
   editUserByIdHandler,
@@ -14,5 +15,6 @@ useRouter.get("/:id", getUserByIdHandler);
 useRouter.post("/", postUserHandler);
 useRouter.put("/:id", editUserByIdHandler);
 useRouter.delete("/:id", deleteUserHandler);
+useRouter.get("/byRole/:role", getUsersByRoleHandler)
 
 module.exports = useRouter;
