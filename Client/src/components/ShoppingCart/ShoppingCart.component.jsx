6c6921a565e974575d9953359052229bfe139ee1
unknown <jgabriel.olivares1994@gmail.com>
2023-12-08 18:33:38 -0300

    ShopCart y LocalStorage

--- Client/src/components/ShoppingCart/ShoppingCart.component.jsx
+++ Client/src/components/ShoppingCart/ShoppingCart.component.jsx
@@ -12,7 +12,7 @@ export default function ShoppingCart() {
   return (
     <TextField
       label="Shopping Cart" // Se proporciona un texto descriptivo sobre el propósito del campo.
-      multiline // Utilizamos multiline para permitir múltiples líneas en el TextField
+      multiline // Utilizamos multiline para permitir múltiples líneas en el TextField.
       rows={4}
       variant="outlined" // borde alrededor del campo de texto.
       fullWidth
