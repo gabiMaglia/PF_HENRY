7be1f8b2b5afe1c0c6951a038abeaeae1061db2c
Gabriel Maglia <gab.maglia@gmail.com>
2023-12-08 13:29:50 -0300

    dividiendo controllors y handlers de user

--- Api/index.js
+++ Api/index.js
@@ -19,11 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const PORT = 3001
+const PORT = 3001;
 
-conn.sync({ force: false  }).then(() => {
-
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
-    console.log(`Server listening at ${PORT}, running on ${process.env.NODE_ENV.toUpperCase()} enviroment` ); // eslint-disable-line no-console
+    console.log(
+      `Server listening at ${PORT}, running on ${process.env.NODE_ENV.toUpperCase()}_DB enviroment`
+    ); // eslint-disable-line no-console
   });
 });
