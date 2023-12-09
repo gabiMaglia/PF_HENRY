a099cff4af16701c6d4f3bb18ff1bd0d22a4fb45
scherpablo <scherpablo@gmail.com>
2023-12-09 17:30:39 -0300

    conectando form con back y correccion de tipografia de success

--- Api/src/handlers/mailingHandler.js
+++ Api/src/handlers/mailingHandler.js
@@ -12,7 +12,7 @@ const supportMailHandler = async (req, res) => {
       content,
       destinationEmail
     );
-    if (response.succes) {
+    if (response.success) {
       return res.status(200).send(response);
     } else {
       return res.status(500).json({ response: `Su mail no pudo ser enviado` });
