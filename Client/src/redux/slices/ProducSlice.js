dc891caf9e6ac3309555ed1a008ab7d5b9354b6f
MariangelRondan <rondanmariangel@gmail.com>
2023-12-10 18:45:57 -0300

    aver

--- Client/src/redux/slices/ProducSlice.js
+++ Client/src/redux/slices/ProducSlice.js
@@ -5,7 +5,7 @@ const url = "http://localhost:3001";
 const initialState = {
   products: [],
   allProducts: [],
-  productById: null,
+  productById: {},
 };
 
 const productSlice = createSlice({
@@ -19,32 +19,32 @@ const productSlice = createSlice({
     getProductById: (state, action) => {
       state.productById = action.payload;
     },
-    search: (state, action) => {
-        state.products = action.payload;
-    }
   },
 });
 
-export const { getProducts, getProductById, search } = productSlice.actions;
+export const { getProducts, getProductById } = productSlice.actions;
 
 export default productSlice.reducer;
 
-export const fetchAllProducts = () => async (dispatch) => {
-  try {
-    const response = await axios.get(`${url}/product/`);
-    dispatch(getProducts(response.data));
-  } catch (error) {
-    alert ("Producto no existente");
-  }
+export const fechAllProducts = () => (dispatch) => {
+  axios
+    .get(`${url}/product/`)
+    .then((response) => {
+      dispatch(getProducts(response.data));
+    })
+    .catch((error) => console.log(error));
 };
 
-export const fetchProductById = (id) => async (dispatch) => {
-  try {
-    const response = await axios.get(`${url}/product/${id}`);
-    dispatch(getProductById(response.data));
-  } catch (error) {
-    console.error("Error fetching product by ID:", error);
-  }
+export const fetchProductById = (id) => (dispatch) => {
+  axios
+    .get(`${url}/product/${id}`)
+    .then((response) => {
+      console.log("Response from fetchProductById:", response.data);
+      dispatch(getProductById(response.data));
+    })
+    .catch((error) => {
+      console.error("Error fetching product by ID:", error);
+    });
 };
 
 export const fetchSearch = (name) => async (dispatch) => {
@@ -52,6 +52,6 @@ export const fetchSearch = (name) => async (dispatch) => {
     const response = await axios.get(`${url}/search?name=${name}`);
     dispatch(search(response.data));
   } catch (error) {
-    alert ("Producto no existente");
+    alert("Producto no existente");
   }
 };
