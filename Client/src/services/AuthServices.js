53cae51ef79d195307079cdc9f0537d0eeb129ac
Gabriel Maglia <gab.maglia@gmail.com>
2023-12-11 20:35:32 -0300

    authServices.js updated

--- Client/src/services/AuthServices.js
+++ Client/src/services/AuthServices.js
@@ -1,21 +1,34 @@
 import axios from "axios";
 const url = import.meta.env.VITE_BACK_URL
 
-export const getUserCredById = async () => {
+export const loginUser = async (username, password) => {
     try {
-        const GetCred = await axios.get(`${url}/user_credentials/:id`);
-        return GetCred;
-      } catch ({ GetCred }) {
-        return { error: GetCred };
+        const loginData = await axios.post(`${url}/account/login`, {
+          username:username,
+          password:password
+        });
+        return loginData;
+      } catch ({response}) {
+        return { error: response };
+      }
+}
+export const googleLoginUser = async () => {
+    try {
+        const loginData = await axios.get(`${url}/auth/google`);
+        return loginData;
+      } catch ({response}) {
+        return { error: response };
       }
 }
 
-export const editUserCredById = async () => {
+export const registerUser = async (userObj) => {
     try {
-        const editCred = await axios.put(`${url}/user_credentials/:id`);
-        return editCred;
-      } catch ({ editCred }) {
-        return { error: editCred };
+        const registerData = await axios.post(`${url}/account/signin`, {
+          userObj
+        });
+        return registerData;
+      } catch ({response}) {
+        return { error: response };
       }
 }
 
