fbfed0f448719cf11268feb6d7b2bcd23d9003b3
Gabriel Maglia <gab.maglia@gmail.com>
2023-12-11 20:45:48 -0300

    login funcionando

--- Client/src/services/AuthServices.js
+++ Client/src/services/AuthServices.js
@@ -1,34 +1,32 @@
 import axios from "axios";
-const url = import.meta.env.VITE_BACK_URL
+const url = import.meta.env.VITE_BACK_URL;
 
 export const loginUser = async (username, password) => {
-    try {
-        const loginData = await axios.post(`${url}/account/login`, {
-          username:username,
-          password:password
-        });
-        return loginData;
-      } catch ({response}) {
-        return { error: response };
-      }
-}
+  try {
+    const loginData = await axios.post(`${url}/account/login`, {
+      username: username,
+      password: password,
+    });
+    return loginData;
+  } catch ({ response }) {
+    return { error: response };
+  }
+};
 export const googleLoginUser = async () => {
-    try {
-        const loginData = await axios.get(`${url}/auth/google`);
-        return loginData;
-      } catch ({response}) {
-        return { error: response };
-      }
-}
-
+  try {
+    const loginData = await axios.get(`${url}/auth/google`);
+    return loginData;
+  } catch ({ response }) {
+    return { error: response };
+  }
+};
 export const registerUser = async (userObj) => {
-    try {
-        const registerData = await axios.post(`${url}/account/signin`, {
-          userObj
-        });
-        return registerData;
-      } catch ({response}) {
-        return { error: response };
-      }
-}
-
+  try {
+    const registerData = await axios.post(`${url}/account/signin`, {
+      userObj,
+    });
+    return registerData;
+  } catch ({ response }) {
+    return { error: response };
+  }
+};
