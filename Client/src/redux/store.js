20d2f933f2888e5160ddc3263ebcc3112aa7c838
Simply92 <pedrouner92@gmail.com>
2023-12-11 22:49:47 -0300

    Funciona!

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
-import productReducer from "./slices/ProducSlice"
+import productReducer from "./slices/ProductSlice"
 
 import thunkMiddleware from 'redux-thunk'
 
