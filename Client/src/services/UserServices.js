927cc8355dc11819d78d2bb67ab4d03ede9d36ff
Martín <martingaliottimartinez@gmail.com>
2023-12-12 18:37:14 -0300

    Renderizado del nombre y apellido cuando inicia sesion

--- Client/src/services/UserServices.js
+++ Client/src/services/UserServices.js
@@ -1,48 +1,48 @@
-import axios from "axios"
-const url = import.meta.env.VITE_BACK_URL
+import axios from "axios";
+const url = import.meta.env.VITE_BACK_URL;
 
 export const getAllUsers = async () => {
-    try {
-      const AllUsers = await axios.get(`${url}/users/`);
-      return AllUsers;
-    } catch ({ AllUsers }) {
-      return { error: AllUsers };
-    }
-  };
+  try {
+    const AllUsers = await axios.get(`${url}/users/`);
+    return AllUsers;
+  } catch ({ AllUsers }) {
+    return { error: AllUsers };
+  }
+};
 
-export const GetUserById = async () => {
-    try {
-        const ById = await axios.get(`${url}/users/:id`)
-        return ById;
-    } catch ({ById}) {
-        return { error: ById };
-    }
+export const getUserById = async (id) => {
+  try {
+    const ById = await axios.get(`${url}/user/${id}`);
+    const { data } = ById;
+    return data;
+  } catch ({ ById }) {
+    return { error: ById };
+  }
 };
 
 export const PostUser = async () => {
-    try {
-        const NewUser = await axios.post(`${url}/users/`)
-        return NewUser;
-    } catch ({NewUser}) {
-        return { error: NewUser };
-    }
-}
+  try {
+    const NewUser = await axios.post(`${url}/users/`);
+    return NewUser;
+  } catch ({ NewUser }) {
+    return { error: NewUser };
+  }
+};
 
 export const PutUser = async () => {
-    try {
-        const PutUser = await axios.put(`${url}/users/:id`)
-        return PutUser;
-    } catch ({ PutUser }) {
-        return { error: PutUser };
-    }
-}
+  try {
+    const PutUser = await axios.put(`${url}/users/:id`);
+    return PutUser;
+  } catch ({ PutUser }) {
+    return { error: PutUser };
+  }
+};
 
 export const DeleteUser = async () => {
-    try {
-        const DeleteUser = await axios.delete(`${url}/users/:id`)
-        return DeleteUser;
-    } catch ({ DeleteUser }) {
-        return { error: DeleteUser };
-    }
-}
-
+  try {
+    const DeleteUser = await axios.delete(`${url}/users/:id`);
+    return DeleteUser;
+  } catch ({ DeleteUser }) {
+    return { error: DeleteUser };
+  }
+};
