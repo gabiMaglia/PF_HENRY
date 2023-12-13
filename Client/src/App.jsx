9731952be8afd74355098d18610bafcae73338d8
scherpablo <scherpablo@gmail.com>
2023-12-13 18:41:49 -0300

    re captcha

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,6 @@
 //HOOKS
 import { Routes, Route } from "react-router-dom";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 //COMPONENTS
 import SearchBar from "./components/SearchBar/SearchBar.component";
 import NavBar from "./components/NavBar/NavBar.component";
@@ -10,29 +11,38 @@ import Products from "./views/Products/Products.view";
 import Support from "./views/Support/Support.view";
 import Questions from "./views/Questions/Questions.view";
 import Detail from "./views/Detail/Detail.view";
+//MATREIAL UI
+import { Box } from "@mui/material";
 //DB
 import PRODUCTS from "./dataBase/bdd.json";
 //HELPERS
 import PATHROUTES from "./helpers/pathRoute";
 
+const reCaptchaKey = import.meta.env.VITE_RECAPTCHA_V3;
+
 const App = () => {
   return (
     <>
-      <div>
-        <SearchBar />
-        <NavBar />
-        <Routes>
-          <Route path={PATHROUTES.HOME} element={<Home />} />
-          <Route path={PATHROUTES.PRODUCTS} element={<Products />} />
-          <Route path={PATHROUTES.SUPPORT} element={<Support />} />
-          <Route path={PATHROUTES.QUESTIONS} element={<Questions />} />
-          <Route
-            path={PATHROUTES.DETAIL}
-            element={<Detail products={PRODUCTS} />}
-          />
-        </Routes>
-        <Footer />
-      </div>
+      <GoogleReCaptchaProvider
+        reCaptchaKey={reCaptchaKey}
+        language="es"
+      >
+        <Box>
+          <SearchBar />
+          <NavBar />
+          <Routes>
+            <Route path={PATHROUTES.HOME} element={<Home />} />
+            <Route path={PATHROUTES.PRODUCTS} element={<Products />} />
+            <Route path={PATHROUTES.SUPPORT} element={<Support />} />
+            <Route path={PATHROUTES.QUESTIONS} element={<Questions />} />
+            <Route
+              path={PATHROUTES.DETAIL}
+              element={<Detail products={PRODUCTS} />}
+            />
+          </Routes>
+          <Footer />
+        </Box>
+      </GoogleReCaptchaProvider>
     </>
   );
 };
