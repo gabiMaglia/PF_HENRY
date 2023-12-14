96b358194f66c2cb31f2ff4fe2cbf288ac26ad24
unknown <jgabriel.olivares1994@gmail.com>
2023-12-14 17:10:36 -0300

    Slice finalizado

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,15 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
-import productReducer from "./slices/ProductSlice"
+import productReducer from "./slices/ProductSlice";
+import cartReducer from "./slices/CartSlice";
 
-import thunkMiddleware from 'redux-thunk'
+import thunkMiddleware from "redux-thunk";
 
 const store = configureStore({
-    reducer: {
-      user: userReducer,
-      product: productReducer,
-    },
-    middleware: [thunkMiddleware],
-})
+  reducer: {
+    user: userReducer,
+    product: productReducer,
+    cart: cartReducer,
+  },
+  middleware: [thunkMiddleware],
+});
 
-export default store; 
+export default store;
