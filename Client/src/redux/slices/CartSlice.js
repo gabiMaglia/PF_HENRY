b159c956b11aed7eb0d947c8cc400acd7deb8782
unknown <jgabriel.olivares1994@gmail.com>
2023-12-17 12:45:09 -0300

    Almacenamiento guardado

--- Client/src/redux/slices/CartSlice.js
+++ Client/src/redux/slices/CartSlice.js
@@ -1,9 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// este código define un slice llamado "cart" con un estado inicial que tiene una propiedad items.
-// Proporciona dos acciones (addItem y checkout) para modificar el estado, un selector (selectItems) para acceder
-// a la propiedad items del estado, y exporta el reducer asociado al slice.
-
 const initialState = {
   items: [],
 };
@@ -16,8 +12,7 @@ const cartSlice = createSlice({
       state.items.push(action.payload);
     },
     checkout: (state) => {
-      //   state.items = [];
-      state;
+      state.items = [];
     },
   },
 });
