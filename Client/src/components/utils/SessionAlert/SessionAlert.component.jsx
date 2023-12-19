5f15c995f770e6015cd8121d51b256c4315d66d9
scherpablo <scherpablo@gmail.com>
2023-12-19 11:34:53 -0300

    updtae varios

--- Client/src/components/utils/SessionAlert/SessionAlert.component.jsx
+++ Client/src/components/utils/SessionAlert/SessionAlert.component.jsx
@@ -4,7 +4,7 @@ const SessionAlert = () => {
   Swal.fire({
     icon: "info",
     title: "Acceso Privado",
-    text: "Debes estar logueado para acceder a esta ruta.",
+    text: "Debes estar logueado para acceder.",
     confirmButtonColor: "#3085d6",
     confirmButtonText: "Ok",
   });
