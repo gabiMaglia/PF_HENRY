a74fb232c198c777e7cad7d64f6fd77d7c691063
valentin di domenica <valendido69@gmail.com>
2023-12-19 17:56:42 -0300

    cambio de modelos

--- Api/src/models/productModels/ProductImage.js
+++ Api/src/models/productModels/ProductImage.js
@@ -9,7 +9,7 @@ module.exports = (sequelize) => {
       primaryKey: true,
     },
     address: {
-      type: DataTypes.DATE,
+      type: DataTypes.STRING,
       allowNull: false,
       unique: true,
     },
