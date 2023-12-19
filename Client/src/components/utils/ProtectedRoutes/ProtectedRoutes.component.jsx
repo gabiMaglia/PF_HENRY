b59c1ceeb9eb108b8b4ed62c216f439b8e4ee2ce
scherpablo <scherpablo@gmail.com>
2023-12-19 11:09:39 -0300

    updtae varios

--- Client/src/components/utils/ProtectedRoutes/ProtectedRoutes.component.jsx
+++ Client/src/components/utils/ProtectedRoutes/ProtectedRoutes.component.jsx
@@ -1,27 +1,13 @@
-import { useEffect } from "react";
-import { Route, useNavigate, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux"; 
 
-import PATHROUTES from "../../../helpers/pathRoute";
+const ProtectedRoutesComponent = ({ redirectPath = "/" }) => {
+  const isAuthenticated = useSelector((state) => state.user.login);
 
-const ProtectedRoutesComponent = ({ canActivate, element }) => {
-  // const userData = useSelector((state) => state.user);
-  // const isLoggedIn = userData.login;
-  const navigate = useNavigate();
-
-  // console.log("Datos del usuario:", userData);
-
-  useEffect(() => {
-    if (!canActivate) {
-      navigate(PATHROUTES.HOME, { replace: true });
-    }
-  }, [canActivate, navigate]);
-
-  return canActivate ? (
-    <Route element={element} />
-  ) : (
-    <Navigate to={PATHROUTES.HOME} />
-  );
+  if (!isAuthenticated) {
+    return <Navigate to={redirectPath} replace />;
+  }
+  return <Outlet />;
 };
 
 export default ProtectedRoutesComponent;
