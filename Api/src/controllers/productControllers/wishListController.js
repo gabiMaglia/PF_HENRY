e2f6703d1d2eae9205efe25943323d6f6eed502c
valentin di domenica <valendido69@gmail.com>
2023-12-19 18:44:06 -0300

    mayor funcionalidad

--- Api/src/controllers/productControllers/wishListController.js
+++ Api/src/controllers/productControllers/wishListController.js
@@ -1,9 +1,7 @@
 const { WishList, User,Product } = require("../../db");
 
 const getWishListController = async () => {
-  const response = await WishList.findAll();
-  console.log(response);
-  console.log(response.length);
+  const response = await WishList.findAll({ include: [{ model: Product }] });
   if (response.length === 0) {
     return {
       error: true,
@@ -13,24 +11,42 @@ const getWishListController = async () => {
   return response;
 };
 
-const postwishItemController=async(userId,productId)=>{ 
-  const user = await User.findOne({ where: { id: userId }, include: WishList })
-  if(!user){
+const postwishItemController = async (userId, productId) => {
+  const user = await User.findOne({ where: { id: userId }, include: WishList });
+  if (!user) {
     return {
       error: true,
-      response: `Usuario no encontrado.`,
+      response: 'Usuario no encontrado.',
     };
   }
+
   const product = await Product.findOne({ where: { id: productId } });
-  if(!product){
+  if (!product) {
     return {
       error: true,
-      response: `Producto no encontrado.`,
+      response: 'Producto no encontrado.',
     };
   }
-  await user.WishList.addProduct(product);
-  const list=await WishList.findAll()
-  return list
+
+  if (user.WishList) {
+    const products = await user.WishList.getProducts();
+    const productExists = products.some(p => p.id === product.id);
+
+    if (productExists) {
+      await user.WishList.removeProduct(product);
+    } else {
+      await user.WishList.addProduct(product);
+    }
+    await user.WishList.save();
+
+    const list = await user.getWishList({ include: [{ model: Product }] });
+    return list;
+  } else {
+    const [wishList] = await WishList.findOrCreate({ where: { UserId: user.id } });
+    await wishList.addProduct(product);
+    const list = await user.getWishList({ include: [{ model: Product }] });
+    return list;
+  }
 }
 
 module.exports = { getWishListController,postwishItemController };
--- Api/src/models/productModels/WishList.js
+++ Api/src/models/productModels/WishList.js
@@ -4,6 +4,8 @@ module.exports = (sequelize) => {
   sequelize.define("WishList", {
     id: {
       type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
       primaryKey: true,
     },
   });
