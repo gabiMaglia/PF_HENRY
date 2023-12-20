0d4a2828b176d3a5382ab94a6074f54b20f82e73
unknown <jgabriel.olivares1994@gmail.com>
2023-12-20 11:38:09 -0300

    Cambio

--- Client/src/components/Detail/Detail.component.jsx
+++ Client/src/components/Detail/Detail.component.jsx
@@ -13,6 +13,7 @@ import {
 import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { resetState } from "../../redux/slices/ProductSlice";
+import { addItemsToCart } from "../../redux/slices/CartSlice";
 import CarouselProducts from "../CarouselProducts/CarouselProducts.component";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -54,7 +55,7 @@ const Detail = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { productById, isLoading } = useSelector((state) => state.product);
-  const [cartItems, setCartItems] = useState([]);
+  const [localCartItems, setLocalCartItems] = useState([]);
   const [selectedImage, setSelectedImage] = useState(null);
   const [loading, setLoading] = useState(true); // Nuevo estado de carga
   const { allProducts } = useSelector((state) => state.product);
@@ -103,11 +104,19 @@ const Detail = () => {
 
   const handleAddToCart = () => {
     if (productById && productById.id) {
-      setCartItems([...cartItems, productById]);
-      console.log("Product added to cart for ID:", productById.id);
-      navigate("/shoppingcart");
+      // Agrega el producto al estado local del carrito
+      setLocalCartItems([...localCartItems, productById]);
+      console.log("Product added to local cart for ID:", productById.id);
     }
   };
+
+  useEffect(() => {
+    // Esta función se ejecuta al desmontar el componente
+    return () => {
+      // Despacha la acción para agregar productos al carrito global
+      dispatch(addItemsToCart(localCartItems));
+    };
+  }, [dispatch, localCartItems]);
 
   const isLargeScreen = useMediaQuery("(min-width:900px)");
   const isSmallScreen = useMediaQuery("(max-width:500px)");
