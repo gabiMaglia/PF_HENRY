c752df0677e8b188160b4597c3fc4f101975911a
scherpablo <scherpablo@gmail.com>
2023-12-22 11:05:56 -0300

    rutas

--- Client/src/components/UserPanel/UserPanel.component.jsx
+++ Client/src/components/UserPanel/UserPanel.component.jsx
@@ -8,6 +8,8 @@ import SideBar from "../SideBar/SideBar.component";
 import UserProfile from "../UserProfile/UserProfile.component";
 import ShopingProfile from "../ShoppingProfile/ShoppingProfile.component";
 import WishListProfile from "../WishListProfile/WishListProfile.component";
+import TechniciansProfile from "../TechniciansProfile/TechniciansProfile.component";
+import ProductCreateProfile from "../ProductCreateProfile/ProductCreateProfile.component";
 import ProductsServicesProfile from "../ProductsServicesProfile/ProductServicesProfile.component";
 //HELPERS
 import PATHROUTES from "../../helpers/pathRoute";
@@ -101,27 +103,26 @@ const UserPanelComponent = () => {
         <Route
           path={
             userRole === "admin"
-              ? PATHROUTES.PRODUCTS_SERVICES
-              : PATHROUTES.PRODUCTS_SERVICES
+              ? PATHROUTES.PRODUCT_CREATE
+              : PATHROUTES.SHOPINGS
           }
-          element={<ProductsServicesProfile />}
+          element={userRole === "admin" ? <ProductCreateProfile/> : <ShopingProfile />}
         />
         <Route
           path={
             userRole === "admin" ? PATHROUTES.TECHNICIANS : PATHROUTES.WISHLIST
           }
-          element={<WishListProfile />}
+          element={userRole === "admin" ? <TechniciansProfile/> :<WishListProfile />}
         />
         <Route
           path={
             userRole === "admin"
-              ? PATHROUTES.PRODUCT_CREATE
-              : PATHROUTES.SHOPINGS
+              ? PATHROUTES.PRODUCTS_SERVICES
+              : PATHROUTES.PRODUCTS_SERVICES
           }
-          element={<ShopingProfile />}
+          element={<ProductsServicesProfile />}
         />
       </Routes>
-
       <Box
         sx={{
           width: "15%",
