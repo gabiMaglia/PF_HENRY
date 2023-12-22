803635739d47b4a27aba7baa0a184d777b2a5f6d
scherpablo <scherpablo@gmail.com>
2023-12-22 18:06:38 -0300

    user menu tecnicos

--- Client/src/components/UserMenu/UserMenu.component.jsx
+++ Client/src/components/UserMenu/UserMenu.component.jsx
@@ -21,6 +21,8 @@ import {
   HomeRepairService,
   Add,
   People,
+  History,
+  Handyman
 } from "@mui/icons-material";
 //HELPERS
 import PATHROUTES from "../../helpers/pathRoute";
@@ -35,12 +37,12 @@ import { logoutUser } from "../../redux/slices/UserSlice";
 
 const UserMenu = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const { name, surname } = useSelector((state) => state.user);
   const initialLetersUsers = {
     name: getFirstLetters(name),
     surname: getFirstLetters(surname),
   };
-  const dispatch = useDispatch();
   const authData = getAuthDataCookie("authData");
   const userRole = authData.userRole;
 
@@ -138,6 +140,45 @@ const UserMenu = () => {
         name: "Productos en servicio",
         icon: <HomeRepairService />,
         path: PATHROUTES.USERPANEL + PATHROUTES.PRODUCTS_SERVICES,
+      },
+      {
+        name: "Cerrar sesion",
+        icon: <Logout />,
+        action: "logout",
+        path: PATHROUTES.HOME,
+      },
+    ];
+  }else if (userRole === "technician") {
+    items = [
+      {
+        name: "Mi cuenta",
+        icon: (
+          <Avatar
+            sx={{ backgroundColor: "#fd611a", height: "40px", width: "40px" }}
+          >
+            <Typography
+              variant="caption"
+              sx={{
+                fontWeight: "bold",
+                color: "white",
+              }}
+            >
+              {initialLetersUsers.name + initialLetersUsers.surname}
+            </Typography>
+          </Avatar>
+        ),
+        path: PATHROUTES.USERPANEL + PATHROUTES.PROFILE,
+      },
+      {
+        name: "Productos en servicio",
+        icon: <HomeRepairService />,
+        path: PATHROUTES.USERPANEL + PATHROUTES.PRODUCTS_SERVICES,
+      },
+      { name: "Prodcutos reparados", icon: <Handyman />, path: PATHROUTES.USERPANEL + PATHROUTES.REAPIRED_PRODCUTS },
+      {
+        name: "Historial reparaciones",
+        icon: <History />,
+        path: PATHROUTES.USERPANEL + PATHROUTES.REPAIR_HISTORY,
       },
       {
         name: "Cerrar sesion",
