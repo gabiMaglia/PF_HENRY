0f03e103445407794b5eb5b7d6267c7f4bdffa99
Gabriel Maglia <93683370+gabiMaglia@users.noreply.github.com>
2023-12-22 21:15:31 -0300

    fixed

--- Api/src/middlewares/googleAuthMiddleware.js
+++ Api/src/middlewares/googleAuthMiddleware.js
@@ -13,7 +13,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.API_URL}/auth/google/callback`,
+      callbackURL: `${process.env.API_URL}auth/google/callback`,
       scope: ["profile", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
