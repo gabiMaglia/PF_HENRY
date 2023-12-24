fcb8c8b5d5b7b2c5065b371b59dda131301e6d04
Martín <martingaliottimartinez@gmail.com>
2023-12-24 12:53:02 -0300

    Prettier

--- Client/src/components/EditModal/EditModal.jsx
+++ Client/src/components/EditModal/EditModal.jsx
@@ -163,10 +163,7 @@ const EditModal = ({
   const formRender = () => {
     return dataList.map((data) => {
       return (
-        <Box
-          key={data.en}
-          sx={{ width: "100%" }}
-        >
+        <Box key={data.en} sx={{ width: "100%" }}>
           <TextField
             fullWidth
             margin="normal"
@@ -200,18 +197,12 @@ const EditModal = ({
                 : errors[data.en] !== ""
             }
           />
-          <FormHelperText
-            error={true}
-            sx={{ fontSize: "0.6em" }}
-          >
+          <FormHelperText error={true} sx={{ fontSize: "0.6em" }}>
             {typeof errors[data.en] !== "object"
               ? errors[data.en]
               : errors[data.en].map((error, key) => {
                   return (
-                    <Typography
-                      variant="p"
-                      key={key}
-                    >
+                    <Typography variant="p" key={key}>
                       {error}
                       <br />
                     </Typography>
@@ -248,26 +239,17 @@ const EditModal = ({
             onClick={resetModal}
           />
         </Button>
-        <Typography
-          sx={{ mb: ".5em" }}
-          variant="h6"
-        >
+        <Typography sx={{ mb: ".5em" }} variant="h6">
           Editar {dataName}
         </Typography>
         {typeof previousValue === "object" ? (
           ""
         ) : (
-          <Typography
-            sx={{ mb: ".5em" }}
-            variant="h5"
-          >
+          <Typography sx={{ mb: ".5em" }} variant="h5">
             {previousValue}
           </Typography>
         )}
-        <Typography
-          variant="body1"
-          sx={{ color: "#fd611a", mb: ".3em" }}
-        >
+        <Typography variant="body1" sx={{ color: "#fd611a", mb: ".3em" }}>
           Para continuar ingresá tus datos actualizados
         </Typography>
         <FormControl
