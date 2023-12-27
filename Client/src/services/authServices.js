5048d77535267247151e384f9542b6c71fc31c9e
Gabriel Maglia <93683370+gabiMaglia@users.noreply.github.com>
2023-12-26 21:03:03 -0300

    eliminado un console.log del authService

--- Client/src/services/authServices.js
+++ Client/src/services/authServices.js
@@ -54,7 +54,7 @@ export const googleLoginUser = async (cookieStatus) => {
       window.addEventListener("message", (event) => {
         if (event.origin === `${url}` && event.data) {
           const sortedData = dataSorterForApp(event.data);
-          console.log(sortedData);
+        
           createPersistency(sortedData, cookieStatus)
           popup.close();
           resolve({ data: event.data });
