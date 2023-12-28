8ca505e43bee6813d4fbb62dcd8fa3187d979438
Simply92 <pedrouner92@gmail.com>
2023-12-27 23:45:47 -0300

    algunos cambios

--- Client/src/components/ShoppingCart/ShoppingCart.component.jsx
+++ Client/src/components/ShoppingCart/ShoppingCart.component.jsx
@@ -1,5 +1,5 @@
 //HOOKS
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
 //MATERIAL UI
@@ -12,14 +12,17 @@ import {
   Button,
 } from "@mui/material";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import QuantityInput from "./InputCount.component";
+import { Unstable_NumberInput as BaseNumberInput } from "@mui/base/Unstable_NumberInput";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
+
 //REDUX
 import {
   addItem,
   updateItem,
   removeItem,
   totalItem,
-} from "../../redux/slices/cartSlice";
+} from "../../redux/slices/CartSlice";
 //SERVICES
 import { fetchCart, fetchGetProduct } from "../../services/productServices";
 
@@ -53,10 +56,27 @@ export default function ShoppingCart() {
     );
   }
 
-  const handleChange = (product, newValue) => {
-    // const newQuantity = parseInt(event.target.value, 10) || 1;
+  const handleChange = (productId, event) => {
+    const newValue = parseInt(event.target.value) || 1;
+    console.log(newValue);
+    dispatch(updateItem({ id: productId, count: newValue }));
+  };
 
-    dispatch(updateItem({ id: product.id, count: newValue }));
+  const handleIncrement = (productId) => {
+    const currentCount = items.find((item) => item.id === productId).count;
+    dispatch(
+      updateItem({
+        id: productId,
+        count: Math.min(currentCount + 1, 10),
+      })
+    );
+  };
+
+  const handleDecrement = (productId) => {
+    const currentCount = items.find((item) => item.id === productId).count;
+    dispatch(
+      updateItem({ id: productId, count: Math.max(currentCount - 1, 1) })
+    );
   };
 
   const handleDelete = (product) => {
@@ -88,10 +108,38 @@ export default function ShoppingCart() {
               src={item.ProductImages.address}
             />
             <Typography>{item.name}</Typography>
-            <QuantityInput
+
+            <BaseNumberInput
+              min={1}
+              max={10}
               id={item.id}
               value={item.count}
-              onChange={(newValue) => handleChange(item, newValue)}
+              onChange={(event) => handleChange(item.id, event.target.value)}
+              slots={{
+                root: StyledInputRoot,
+                input: StyledInput,
+                incrementButton: StyledButton,
+                decrementButton: StyledButton,
+              }}
+              slotProps={{
+                incrementButton: {
+                  children: (
+                    <AddIcon
+                      fontSize="small"
+                      onClick={() => handleIncrement(item.id)}
+                    />
+                  ),
+                  className: "increment",
+                },
+                decrementButton: {
+                  children: (
+                    <RemoveIcon
+                      fontSize="small"
+                      onClick={() => handleDecrement(item.id)}
+                    />
+                  ),
+                },
+              }}
             />
             <Typography>Precio: ${item.price * item.count}</Typography>
             <Button onClick={() => handleDelete(item.id)}>
@@ -131,3 +179,114 @@ export default function ShoppingCart() {
     </Container>
   );
 }
+
+const blue = {
+  100: "#daecff",
+  200: "#b6daff",
+  300: "#66b2ff",
+  400: "#3399ff",
+  500: "#007fff",
+  600: "#0072e5",
+  700: "#0059B2",
+  800: "#004c99",
+};
+
+const grey = {
+  50: "#F3F6F9",
+  100: "#E5EAF2",
+  200: "#DAE2ED",
+  300: "#C7D0DD",
+  400: "#B0B8C4",
+  500: "#9DA8B7",
+  600: "#6B7A90",
+  700: "#434D5B",
+  800: "#303740",
+  900: "#1C2025",
+};
+
+const StyledInputRoot = styled("div")(
+  ({ theme }) => `
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-weight: 400;
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[500]};
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+`
+);
+
+const StyledInput = styled("input")(
+  ({ theme }) => `
+  font-size: 0.875rem;
+  font-family: inherit;
+  font-weight: 400;
+  line-height: 1.375;
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+  box-shadow: 0px 2px 4px ${
+    theme.palette.mode === "dark" ? "rgba(0,0,0, 0.5)" : "rgba(0,0,0, 0.05)"
+  };
+  border-radius: 8px;
+  margin: 0 8px;
+  padding: 10px 12px;
+  outline: 0;
+  min-width: 0;
+  width: 4rem;
+  text-align: center;
+
+  &:hover {
+    border-color: ${blue[400]};
+  }
+
+  &:focus {
+    border-color: ${blue[400]};
+    box-shadow: 0 0 0 3px ${
+      theme.palette.mode === "dark" ? blue[700] : blue[200]
+    };
+  }
+
+  &:focus-visible {
+    outline: 0;
+  }
+`
+);
+
+const StyledButton = styled("button")(
+  ({ theme }) => `
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-size: 0.875rem;
+  box-sizing: border-box;
+  line-height: 1.5;
+  border: 1px solid;
+  border-radius: 999px;
+  border-color: ${theme.palette.mode === "dark" ? grey[800] : grey[200]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : grey[50]};
+  color: ${theme.palette.mode === "dark" ? grey[200] : grey[900]};
+  width: 32px;
+  height: 32px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 120ms;
+
+  &:hover {
+    cursor: pointer;
+    background: ${theme.palette.mode === "dark" ? blue[700] : blue[500]};
+    border-color: ${theme.palette.mode === "dark" ? blue[500] : blue[400]};
+    color: ${grey[50]};
+  }
+
+  &:focus-visible {
+    outline: 0;
+  }
+
+  &.increment {
+    order: 1;
+  }
+`
+);
