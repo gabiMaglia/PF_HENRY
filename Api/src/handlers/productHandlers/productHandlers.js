4982144d2b5e2c8aaeeacb0df404b9f2cbc6d72c
MariangelRondan <rondanmariangel@gmail.com>
2023-12-29 10:20:35 -0300

    alerts en español

--- Api/src/handlers/productHandlers/productHandlers.js
+++ Api/src/handlers/productHandlers/productHandlers.js
@@ -1,5 +1,3 @@
-const { Product } = require("../../db");
-
 const {
   getAllProducts,
   postProduct,
@@ -35,7 +33,7 @@ const postProductHandler = async (req, res) => {
       !images ||
       !soldCount
     ) {
-      return res.status(400).json({ error: "Missing required data..." });
+      return res.status(400).json({ error: "Faltan datos requeridos..." });
     }
     const newProduct = await postProduct({
       name,
@@ -52,7 +50,7 @@ const postProductHandler = async (req, res) => {
 
     res
       .status(201)
-      .json({ message: "Product created successfully", Product: newProduct });
+      .json({ message: "Producto creado correctamente!", Product: newProduct });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -85,7 +83,7 @@ const deleteProductHandler = async (req, res) => {
   const { id } = req.params;
   try {
     const deletedProduct = await deleteProduct(id);
-    res.status(200).json(`Product deleted successfully`);
+    res.status(200).json(`El producto con ID: ${id} fue eliminado`);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -99,7 +97,9 @@ const getProductByIdHandler = async (req, res) => {
     if (product) {
       res.status(200).json(product);
     } else {
-      res.status(404).json({ error: `Product ${id} was not found.` });
+      res
+        .status(404)
+        .json({ error: `Producto con ID: ${id} no fue encontrado.` });
     }
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -114,7 +114,9 @@ const searchByNameHandler = async (req, res) => {
     if (results.length > 0) {
       res.status(200).json(results);
     } else {
-      res.status(404).json({ error: `No products found` });
+      res
+        .status(404)
+        .json({ error: `No se encontraron resultados para ${name}` });
     }
   } catch (error) {
     res.status(400).json({ error: error.message });
