73dcd80b4f8f5ca28136dae42d83f5c9309e68a1
MariangelRondan <rondanmariangel@gmail.com>
2023-12-29 00:15:47 -0300

    stock luego de compra finalizada

--- Api/src/controllers/pagosControllers/mercadoPagoContoller.js
+++ Api/src/controllers/pagosControllers/mercadoPagoContoller.js
@@ -1,7 +1,9 @@
+const { Order, Product, OrderProduct } = require("../../db");
 const { MercadoPagoConfig, Preference } = require("mercadopago");
 const frontend_Url = process.env.FRONTEND_URL;
 const miAccessToken = process.env.MP_ACCESS_TOKEN;
-
+const mercadopago = require("mercadopago");
+const OrderProduct = require("../../models/productModels/OrderProduct");
 const client = new MercadoPagoConfig({ accessToken: miAccessToken });
 
 const mercadoPago = async (array, idOrder) => {
@@ -28,14 +30,64 @@ const mercadoPago = async (array, idOrder) => {
   }
 };
 
-const webhook = async (paymentId, status) => {
-  if (status === "approved") {
-    console.log(`Pago aprobado: ${paymentId}`);
-  } else {
-    console.log(`Estado de pago no manejado: ${status}`);
+const handlePaymentNotification = async (paymentId) => {
+  try {
+    const paymentInfo = await mercadopago.payment.get(paymentId);
+
+    if (paymentInfo.status === "approved") {
+      const orderId = paymentInfo.metadata;
+      const order = await Order.findByPk(orderId, {
+        include: [
+          {
+            model: Product,
+            attributes: ["id", "name"],
+            through: {
+              model: OrderProduct,
+              attributes: ["quantity"],
+            },
+          },
+        ],
+      });
+
+      if (order) {
+        order.status = "finalizado";
+        await order.save();
+
+        const products = order.getProducts();
+
+        for (const product of products) {
+          const { id, stock, soldCount } = product;
+          const orderProduct = order.OrderProducts.find(
+            (op) => op.ProductId === id
+          );
+
+          if (orderProduct) {
+            const productQuantity = orderProduct.quantity;
+
+            stock -= productQuantity;
+
+            soldCount += productQuantity;
+
+            await product.save();
+          }
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error al manejar la notificación de pago:", error);
   }
 };
 
+// };
+// const webhook = async (paymentId, status) => {
+//   if (status === "approved") {
+//     console.log(`Pago aprobado: ${paymentId}`);
+//   } else {
+//     console.log(`Estado de pago no manejado: ${status}`);
+//   }
+// };
+
 module.exports = {
   mercadoPago,
+  handlePaymentNotification,
 };
