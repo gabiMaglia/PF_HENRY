7e779f307c36c3217e5c274183a76fa5f815b96d
scherpablo <scherpablo@gmail.com>
2024-01-02 17:08:10 -0300

    ProductsTable

--- Client/src/components/ProductsTable/ProductsTable.component.jsx
+++ Client/src/components/ProductsTable/ProductsTable.component.jsx
@@ -1,84 +1,79 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Box } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
-const columns = [
-  { field: "id", headerName: "ID", minWidth: 70 },
-  { field: "firstName", headerName: "Nombre", width: 150 },
-  { field: "price", headerName: "Precio", width: 80 },
-  {
-    field: "warranty",
-    headerName: "Garantía",
-    type: "number",
-    width: 100,
-  },
-  {
-    field: "souldCount",
-    headerName: "Vendidos",
-    width: 80,
-  },
-  {
-    field: "brand",
-    headerName: "Marca",
-    width: 100,
-  },
-  {
-    field: "category",
-    headerName: "Categoria",
-    width: 100,
-  },
-];
+const ProductsTable = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-const rows = [
-  {
-    id: 1,
-    firstName: "Monitor Lg",
-    price: 120000,
-    warranty: "6 meses",
-    souldCount: 20,
-  },
-  {
-    id: 2,
-    firstName: "Mother Asus",
-    price: 120000,
-    warranty: "12 meses",
-    souldCount: 8,
-  },
-  {
-    id: 3,
-    firstName: "Teclado Logitech",
-    price: 120000,
-    warranty: "3 meses",
-    souldCount: 25,
-  },
-  {
-    id: 4,
-    firstName: "Mouse HyperX",
-    price: 120000,
-    warranty: "12 meses",
-    souldCount: 46,
-  },
-  {
-    id: 5,
-    firstName: "Gabinete Antec",
-    price: 120000,
-    warranty: "6 meses",
-    souldCount: 12,
-  },
-];
+  const urlBack = import.meta.env.VITE_BACKEND_URL;
 
-export default function DataTable() {
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(`${urlBack}/product/`, {
+        withCredentials: true,
+      });
+      setProducts(response.data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      setError("Error al obtener productos");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const productsWithBrandAndCategory = products.map((product) => {
+    const brand =
+      product.ProductBrands.length > 0
+        ? product.ProductBrands[0].name
+        : "Sin marca";
+    const category =
+      product.ProductCategories.length > 0
+        ? product.ProductCategories[0].name
+        : "Sin categoría";
+
+    return {
+      ...product,
+      brand,
+      category,
+    };
+  });
+
+  const columns = [
+    // { field: "id", headerName: "ID", minWidth: 70 },
+    { field: "name", headerName: "Nombre", width: 350 },
+    { field: "price", headerName: "Precio", width: 80 },
+    { field: "warranty", headerName: "Garantía", type: "number", width: 100 },
+    { field: "soldCount", headerName: "Vendidos", width: 80 },
+    { field: "brand", headerName: "Marca", width: 150 },
+    { field: "category", headerName: "Categoría", width: 150 },
+  ];
+
+  if (loading) {
+    return <p>Cargando productos...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <Box sx={{ width: "100%", height: 400 }}>
       <DataGrid
-        rows={rows}
+        rows={productsWithBrandAndCategory}
         columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
+        pageSize={5}
         checkboxSelection
+        sx={{ minHeight: "800px", marginTop: "50px", textAlign: "center" }}
       />
-    </div>
+    </Box>
   );
-}
+};
+
+export default ProductsTable;
