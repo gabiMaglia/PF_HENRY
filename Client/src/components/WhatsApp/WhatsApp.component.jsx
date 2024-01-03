21db39a72190cd9d157bcd98c4a3a585ed1bb1bb
scherpablo <scherpablo@gmail.com>
2024-01-03 16:49:45 -0300

    centrando columnas

--- Client/src/components/WhatsApp/WhatsApp.component.jsx
+++ Client/src/components/WhatsApp/WhatsApp.component.jsx
@@ -1,11 +1,8 @@
-//HOOKS
-import { useState } from "react";
 //MATERIAL UI
-import { Box, Button, Popover, Typography } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 const WhatsAppComponent = () => {
-  const [popoverOpen, setPopoverOpen] = useState(false);
   const phoneNumber = "+5491132069043";
   const message = "Hola, quiero saber mas acerca del producto...";
 
@@ -13,11 +10,6 @@ const WhatsAppComponent = () => {
     const url = `https://wa.me/${phoneNumber}?text=${message}`;
     window.open(url, "_blank");
   };
-
-  const handlePopoverToggle = () => {
-    setPopoverOpen((prev) => !prev);
-  };
-
 
   return (
     <>
@@ -35,8 +27,6 @@ const WhatsAppComponent = () => {
           variant="contained"
           size="large"
           onClick={handleWhatsAppClick}
-          onClickCapture={handlePopoverToggle}
-          aria-describedby="whatsapp-popover"
           sx={{
             width: "50px",
             borderRadius: "50%",
@@ -46,23 +36,6 @@ const WhatsAppComponent = () => {
         >
           <WhatsAppIcon sx={{ fontSize: "50px" }} />
         </Button>
-        <Popover
-          id="whatsapp-popover"
-          open={popoverOpen}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "left",
-          }}
-          transformOrigin={{
-            vertical: "bottom",
-            horizontal: "left",
-          }}
-          onClose={() => setPopoverOpen(false)}
-        >
-          <Typography sx={{ padding: 2 }}>
-            ¡Hola! ¿En qué puedo ayudarte?
-          </Typography>
-        </Popover>
       </Box>
     </>
   );
