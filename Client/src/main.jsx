8039d1ae6b4ecc960d89f913cada9adfe90fd85f
Gabriel Maglia <93683370+gabiMaglia@users.noreply.github.com>
2024-01-04 16:06:52 -0300

    themeProvider added

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { CssBaseline } from "@mui/material";
-import { Provider } from "react-redux";
+import { CssBaseline, Provider, ThemeProvider } from "react-redux";
 import store from "./redux/store";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -11,7 +10,7 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { theme } from "./utils/themeProvider.js";
-import { ThemeProvider } from "@mui/material";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
