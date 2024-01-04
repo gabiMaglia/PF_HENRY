9b182459c30da6265d4af04bca9f99960306b5f5
Gabriel Maglia <93683370+gabiMaglia@users.noreply.github.com>
2024-01-04 15:57:30 -0300

    themeProvider added

--- Client/src/utils/themeProvider.js
+++ Client/src/utils/themeProvider.js
@@ -1,16 +1,21 @@
-import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
+import { createTheme } from '@mui/material';
 
-// Define tu tema personalizado con la tipografía deseada
 export const theme = createTheme({
   typography: {
     h1: {
-      fontFamily: 'Roboto, sans-serif',
-      // Otras propiedades de estilo para h1 si es necesario
+      fontFamily: 'Roboto, sans-serif'
     },
     h2: {
       fontFamily: 'Times New Roman, serif',
-      // Otras propiedades de estilo para h2 si es necesario
     },
-    // Otras configuraciones de tipografía según sea necesario
+    body1: {
+        
+    },
+    body2: {
+
+    },
+    span : {
+
+    },
   },
 });
