58cc77b74ce32ebf9d0abe05889d78977442a154
valentin di domenica <valendido69@gmail.com>
2024-01-04 22:42:43 -0300

    commit extra

--- Client/src/components/ServiceList/ServiceList.componet.jsx
+++ Client/src/components/ServiceList/ServiceList.componet.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import {useEffect,useState} from 'react'
+import { Box } from '@mui/material'
+import {
+  GridCellEditStopReasons,
+  GridLogicOperator,
+  esES,
+} from "@mui/x-data-grid";
+
+const columns=[
+  {
+    field:"id",headerName:"ID", minWidth: 300, headerAlign: "center"
+  },
+  {
+    field:"product_model",headerName:"Modelo", minWidth: 300, headerAlign: "center"
+  },
+  {
+    field:"product_income_date",headerName:"fecha de ingreso", minWidth: 300, headerAlign: "center"
+  },
+    {
+    field:"userId",headerName:"Usuario", minWidth: 300, headerAlign: "center"
+  },
+    {
+    field:"technicianId",headerName:"Tecnico asignado", minWidth: 300, headerAlign: "center"
+  },
+    {
+    field:"final_diagnosis",headerName:"ID", minWidth: 300, headerAlign: "center"
+  },
+
+]
+
 
 const ServiceList = () => {
   return (
-    <div>ServiceList.componet</div>
+   <Box>lista en proceso<br></br>
+   <br></br>
+   <br></br>
+   <br></br>
+   <br></br>
+    UN BESO
+   </Box>
   )
 }
 
