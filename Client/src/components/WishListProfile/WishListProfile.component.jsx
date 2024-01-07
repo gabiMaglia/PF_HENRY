e0457315de4fad3ac51e8ec423e83343cabdec99
Martín <martingaliottimartinez@gmail.com>
2024-01-07 11:30:46 -0300

    Arreglo Wishlist

--- Client/src/components/WishListProfile/WishListProfile.component.jsx
+++ Client/src/components/WishListProfile/WishListProfile.component.jsx
@@ -113,6 +113,10 @@ const WhishListProfileComponent = () => {
     }
   }, [wishListCards && wishListCards[0] && wishListCards[0].ProductImages]);
 
+  useEffect(() => {
+    wishListCards?.length === 0 && setIsLoading(false);
+  }, [wishListCards]);
+
   const deleteProduct = (id) => {
     setIsLoading(true);
     fetchAddItemWish(dispatch, userId, id); // ELiminar un producto
