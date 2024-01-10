e9d8cd6acb072ab2c63c36745dd9e19c1acfec93
unknown <jgabriel.olivares1994@gmail.com>
2024-01-10 17:28:59 -0300

    vista

---
+++ Client/src/views/publics/Reviews/Reviews.view.jsx
@@ -0,0 +1,11 @@
+import ReviewsComponent from "../../../components/Reviews/Reviews.component";
+
+const ReviewsView = () => {
+  return (
+    <>
+      <ReviewsComponent />
+    </>
+  );
+};
+
+export default ReviewsView;
--- Client/src/views/publics/Support/Support.view.jsx
+++ Client/src/views/publics/Support/Support.view.jsx
@@ -1,11 +1,9 @@
 import SupportComponent from "../../../components/Support/Support.component";
-import ReviewsComponent from "../../../components/Reviews/Reviews.component";
 
 const SupportView = () => {
   return (
     <>
       <SupportComponent />
-      <ReviewsComponent />
     </>
   );
 };
