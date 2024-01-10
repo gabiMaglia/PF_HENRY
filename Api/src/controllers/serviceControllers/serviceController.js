48154c4c8eb3a961f7987ecad0110193f10f72a6
scherpablo <scherpablo@gmail.com>
2024-01-10 16:58:08 -0300

    link a imagen footer mail de servicios

--- Api/src/controllers/serviceControllers/serviceController.js
+++ Api/src/controllers/serviceControllers/serviceController.js
@@ -85,7 +85,8 @@ const addServiceController = async (
             to: clientObj.email, // list of receivers
             subject: "ingreso a servicio ✔",
             html: `Estimado ${clientName}.<br> Le informamos que su equipo se ingreso a nuestro sistema el dia ${date}.<br> El mismo será evaluado por el técnico asignado para el servicio.<br> Una vez evaluado, recibirá por este medio el diagnóstico del mismo y el presupuesto para su reparación.<br> También podrá seguir el estado del servicio desde nuestro <a href="${hypermegared}">Sitio Web</a> ingresando a su panel de usuario, productos en servicio.<br> Ahí podrá ACEPTAR o RECHAZAR el presupuesto.<br> Ante cualquier duda no dude en comunicarse con nuestro sector de soporte técnico. Muchas gracias....<br><br> 
-           <img src='https://res.cloudinary.com/hypermegared/image/upload/v1704231317/wsum710gbvcgjo2ktujm.jpg'/>`,
+            <a href="${hypermegared}"><img src='https://res.cloudinary.com/hypermegared/image/upload/v1704231317/wsum710gbvcgjo2ktujm.jpg'/></a>
+            `,
           });
         }
 
@@ -136,7 +137,7 @@ const updateServiceStatusController = async (id, field, value) => {
         to: clientObj.email,
         subject: "actualizacion de estado ✔",
         html: `Estimado ${clientName}<br> Le informamos que se modificó el estado de su equipo ${service.product_model} a ${field}: ${value}.<br> Recuerde que también puede seguir el estado del mismo desde nuestro <a href="${hypermegared}">Sitio Web</a> ingresando a su panel de usuario, productos en servicio.<br> Ante cualquier duda no dude en comunicarse con nuestro sector de soporte técnico. Muchas gracias....<br><br>
-        <img src='https://res.cloudinary.com/hypermegared/image/upload/v1704231317/wsum710gbvcgjo2ktujm.jpg'/>`,
+        <a href="${hypermegared}"><img src='https://res.cloudinary.com/hypermegared/image/upload/v1704231317/wsum710gbvcgjo2ktujm.jpg'/></a>`,
       });
     }
     return `${field} actualizado a ${value}`;
@@ -161,9 +162,13 @@ const getAllServicesController = async () => {
       const clientObj = await User.findByPk(service.userId);
       const technicianObj = await User.findByPk(service.technicianId);
 
-      const clientName = clientObj ? `${clientObj.name} ${clientObj.surname}` : null;
+      const clientName = clientObj
+        ? `${clientObj.name} ${clientObj.surname}`
+        : null;
       const clientEmail = clientObj ? clientObj.email : null;
-      const technicianName = technicianObj ? `${technicianObj.name} ${technicianObj.surname}` : null;
+      const technicianName = technicianObj
+        ? `${technicianObj.name} ${technicianObj.surname}`
+        : null;
 
       const serviceWithNames = await Service.findByPk(service.id, {
         include: [Service_status, Service_image],
