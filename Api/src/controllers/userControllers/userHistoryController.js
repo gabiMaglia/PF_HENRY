beaee361de74cbe9aab6bc0fc6d8b51eb2239329
valentin di domenica <valendido69@gmail.com>
2024-01-12 20:04:08 -0300

    get y logicalDelete

--- Api/src/controllers/userControllers/userHistoryController.js
+++ Api/src/controllers/userControllers/userHistoryController.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const { User, History } = require("../../db.js");
 
 const PostHistoryController = async (UserId, value) => {
@@ -5,6 +6,12 @@ const PostHistoryController = async (UserId, value) => {
     return {
       error: true,
       response: "no fue posible extraer los datos",
+    };
+  }else if((value.trim() !== "")===false){
+    console.log(value.trim()!=='')
+    return {
+      error: true,
+      response: "no es posible ingresar ese valor.",
     };
   }
   const user = await User.findByPk(UserId);
@@ -21,10 +28,11 @@ const PostHistoryController = async (UserId, value) => {
     }
   });
   if (existingHistory) {
-    return {
-      error: true,
-      response: "ya existe un historial con este valor para este usuario",
-    };
+    if(existingHistory.isDelete===true){
+      existingHistory.isDelete=false
+      await existingHistory.save()
+    }
+    return existingHistory
   }
   const newHistory = await History.create({
     value: value,
@@ -39,4 +47,44 @@ const PostHistoryController = async (UserId, value) => {
   return newHistory;
 };
 
-module.exports = { PostHistoryController };
+const getHistoryUserController=async(id)=>{
+  console.log(id)
+  const user=await User.findByPk(id)
+  if(!user){
+    return {
+      error: true,
+      response: "el usuario no existe.",
+    };
+  }
+  const historyUser=await History.findAll({
+    where:{UserId:id}
+  })
+    if(!historyUser||historyUser.length===0){
+      return('el usuario aun no posee historial.')
+    }
+    return historyUser
+}
+
+const deleteHistoryController=async(id,value)=>{
+  const userHistory=await getHistoryUserController(id)
+  if(userHistory.error){
+    return {
+      error: true,
+      response: "algo salio mal.",
+    }
+  }
+  const historyValue= await History.findOne({
+    where:{
+      UserId:id,
+      value:value
+    }
+  })
+  historyValue.isDelete=true
+  await historyValue.save()
+   const response=await History.findAll({
+    where:{UserId:id}
+   })
+   return response
+}
+
+module.exports = { PostHistoryController,getHistoryUserController,deleteHistoryController };
