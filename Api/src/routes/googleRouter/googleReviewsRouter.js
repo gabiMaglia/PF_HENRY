2e79b43f05eecfa941d36130bd3f120cbfb1227f
MariangelRondan <rondanmariangel@gmail.com>
2024-01-14 12:52:12 -0300

    ruta get para obtener precio de envio. recibe direccion de destino por query

--- Api/src/routes/googleRouter/googleReviewsRouter.js
+++ Api/src/routes/googleRouter/googleReviewsRouter.js
@@ -6,6 +6,12 @@ const {
   getGoogleReviewsHandler,
 } = require("../../handlers/googleHandlers/googleReviewsHandlers");
 
+const {
+  getPrecioEnviosHandler,
+} = require("../../handlers/googleHandlers/googlePlacesHandler");
+
 googleRouter.get("/google-reviews", getGoogleReviewsHandler);
+
+googleRouter.get("/envios", getPrecioEnviosHandler);
 
 module.exports = googleRouter;
