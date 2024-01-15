263698ffe2e526005538a2814c1a0e2f17d0b3e1
Martín <martingaliottimartinez@gmail.com>
2024-01-15 00:53:49 -0300

    Arreglo de importacion en variable de entorno

--- Client/src/services/reportingAnalyticsServices.js
+++ Client/src/services/reportingAnalyticsServices.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 const PROPERTY_ID = import.meta.env.VITE_REPORTING_ANALYTICS_PROYECT_ID;
-const frontUrl = import.meta.env.VITE_FRONT_URL;
 
 export const fetchAnalyticsData = async (accessToken, startDate, endDate) => {
   try {
@@ -12,6 +11,25 @@ export const fetchAnalyticsData = async (accessToken, startDate, endDate) => {
       metrics,
       dimensions,
     };
+
+    // const dimensions = [{ name: "eventName" }, { name: "date" }];
+    // const metrics = [{ name: "eventCount" }];
+    // const dimensionFilter = {
+    //   filter: {
+    //     fieldName: "eventName",
+    //     stringFilter: {
+    //       value: "create_service", // Filtra por el nombre del evento
+    //       matchType: "EXACT",
+    //     },
+    //   },
+    // };
+
+    // const requestBody = {
+    //   dateRanges: [{ startDate, endDate }],
+    //   metrics,
+    //   dimensions,
+    //   dimensionFilter,
+    // };
 
     const headers = {
       "Content-Type": "application/json",
