c22121c19c718985dbc6f3ccae78f60856b7a6bf
scherpablo <scherpablo@gmail.com>
2024-01-20 23:50:56 -0300

    min heigth a preguntas frecuentes

--- Client/src/components/Questions/Questions.component.jsx
+++ Client/src/components/Questions/Questions.component.jsx
@@ -5,37 +5,37 @@ import DropdownStoreService from "../DropdownService/DropdownService.component";
 const QuestionsComponent = () => {
   return (
     <>
-      {/* BOX TITULO */}
-      <Box
-        sx={{
-          backgroundColor: "#000",
-          width: "100%",
-          height: "120px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography
-          variant="h4"
+      <Box sx={{ minHeight: "70vh", marginBottom: "25px" }}>
+        <Box
           sx={{
-            margin: "10px",
-            color: "#fff",
-            textAlign: "center",
-            fontSize: "1.7rem",
-            textTransform: "uppercase",
-            fontWeight: "800",
-            "@media (max-width: 480px)": {
-              transform: "scale(.8)",
-            },
+            backgroundColor: "#000",
+            width: "100%",
+            height: "120px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          Preguntas Frecuentes
-        </Typography>
+          <Typography
+            variant="h4"
+            sx={{
+              margin: "10px",
+              color: "#fff",
+              textAlign: "center",
+              fontSize: "1.7rem",
+              textTransform: "uppercase",
+              fontWeight: "800",
+              "@media (max-width: 480px)": {
+                transform: "scale(.8)",
+              },
+            }}
+          >
+            Preguntas Frecuentes
+          </Typography>
+        </Box>
+        <DropdownStoreComponent />
+        <DropdownStoreService />
       </Box>
-      {/* CIERRE BOX TITULO */}
-      <DropdownStoreComponent />
-      <DropdownStoreService />
     </>
   );
 };
