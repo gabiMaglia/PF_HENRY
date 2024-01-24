735f5b0f91060da60256f9fe68dfcb11b1a6a807
Gabriel Maglia <93683370+gabiMaglia@users.noreply.github.com>
2024-01-24 12:12:53 -0300

    navFIxed y scrollTopBtnFixed

--- Client/src/components/ButtonScrollTop/ButtonScrollTop.component.jsx
+++ Client/src/components/ButtonScrollTop/ButtonScrollTop.component.jsx
@@ -1,30 +1,41 @@
-//HOOKS
+// HOOKS
 import { useState, useEffect } from "react";
-//MATERIAL UI
+// MATERIAL UI
 import { Box, Button } from "@mui/material";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 const ButtonScrollTopComponent = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-  const handleScroll = () => {
+  // MANEJA LA VISIBILIDAD DEL BOTON
+  const handleVisibility = () => {
     const isScrolled = window.scrollY > 400;
     setIsVisible(isScrolled);
   };
-  
+
+  // MANEJA LA ACCION QUE TE LLEVA HACIA ARRIBA
   const handleScrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   };
+
   useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+      handleVisibility(); 
+    };
+    console.log(scrollPosition)
+    console.log(window.scrollY)
     window.addEventListener("scroll", handleScroll);
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [window]);
-  
+  }, []); 
+
   return (
     <>
       <Box
@@ -34,7 +45,7 @@ const ButtonScrollTopComponent = () => {
           left: "60px",
           zIndex: 1000,
           display: isVisible ? "block" : "none",
-          "@media (max-width: 1140px)":{ left: "25px", bottom: "30px" },
+          "@media (max-width: 1140px)": { left: "25px", bottom: "30px" },
         }}
       >
         <Button
